bc89d0cdb9f211c2aaff40823a7a53aaf01c29ea
islamafreed <islamafreed@gmail.com>
2025-11-25 22:09:13 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -29,7 +29,7 @@ export function HeroSection() {
 <div
   className="absolute inset-0 bg-cover bg-center opacity-100"
   style={{
-    backgroundImage: "url('hero.png')",
+    backgroundImage: "url('/hero.png')",
   }}
 />
 
